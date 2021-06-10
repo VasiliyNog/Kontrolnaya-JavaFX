36ead878f8928028dde7fcb4fcc604899bcf08a9
Ноговицын Василий <66771375+VasiliyNog@users.noreply.github.com>
2021-06-11 01:19:04 +0900

    Add files via upload

---
+++ Kontrolnaya JavaFX/src/Controller.java
@@ -0,0 +1,14 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public PasswordField passwordField;
+    public TextField loginTextField;
+    public Button enterButton;
+    public Label statusLabel;
+
+    public void enterButtonClick() {
+    }
+}
